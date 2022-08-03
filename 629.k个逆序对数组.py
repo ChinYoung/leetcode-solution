ecb8837298fff7edc6242c1e331f1c62d9b96a16
ChinYoung <woodichin@outlook.com>
2022-08-03 17:54:08 +0800

    WIP

---
+++ 629.k个逆序对数组.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=629 lang=python3
+#
+# [629] K个逆序对数组
+#
+
+# @lc code=start
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        pass
+
+    def isValid(self, array, k):
+        total = 0
+        for i in array:
+            pass
+# @lc code=end
+
