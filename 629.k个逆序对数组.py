b927c577be75e04da97118d281f4f34103d366ab
ChinYoung <woodichin@outlook.com>
2022-08-04 11:28:51 +0800

    WIP

--- 629.k个逆序对数组.py
+++ 629.k个逆序对数组.py
@@ -6,12 +6,40 @@
 
 # @lc code=start
 class Solution:
+    def __init__(self) -> None:
+        self.log = {}
+    
     def kInversePairs(self, n: int, k: int) -> int:
-        pass
+        if k == 0:
+            return 1
+        if n == 1:
+            return 0
+        try:
+            res = self.log[n][k]
+            return res
+        except KeyError:
+            # f(n, k) = f(n-1, k) + f(n-1, k-1) + f(n-1, k-2) + ..... + f(n-1, k-n+1)
+            # f(n, k+1) = f(n-1, k+1) + f(n-1, k) + f(n-1, k-1) + ..... + f(n-1, k+1-n+1)
+            # =>
+            # f(n, k+1) - f(n,k) = f(n-1, k+1) - f(n-1, k-n+1)
+            # =>
+            # f(n, k+1) = f(n,k) + f(n-1, k+1) - f(n-1, k-n+1)
+            # =>
+            # f(n, k) = f(n,k-1) + f(n-1, k) - f(n-1, k-n)
+            a = self.kInversePairs(n, k-1)
+            b = self.kInversePairs(n-1, k)
+            c = self.kInversePairs(n-1, k-n) if k>n-1 else 0
+            # print(a,b,c)
+            count = a+b-c
+            nLog = self.log.setdefault(n, {})
+            nLog[k] = count
+            return count % (pow(10,9) + 7)
 
-    def isValid(self, array, k):
-        total = 0
-        for i in array:
-            pass
 # @lc code=end
+if __name__ == "__main__":
+    s = Solution()
+    print(s.kInversePairs(2,2))
+    print(s.kInversePairs(3,0))
+    print(s.kInversePairs(3,1))
+    print(s.kInversePairs(4,2))
 
