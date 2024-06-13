7f7ed45fa8fd358c9f89d97e5e7a95e0e7c41ff7
yang qin <73608+YANQIN1@users.noreply.git.i.mercedes-benz.com>
2024-06-13 12:41:25 +0800

    feat: 19 wip

---
+++ 19.删除链表的倒数第-n-个结点.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=19 lang=python3
+#
+# [19] 删除链表的倒数第 N 个结点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        pass
+# @lc code=end
+
